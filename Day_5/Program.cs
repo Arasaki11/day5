babde336177ab2531716afa2fd2884fd2a87acee
Кондрашов максим <113707412+123656756@users.noreply.github.com>
2023-06-16 10:14:43 +0300

    First commit

---
+++ Day_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeBook ebs = new EmployeeBook();
+
+             ebs.TargetA();
+             ebs.TargetB();
+             ebs.TargetC();
+             ebs.TargetD();
+             ebs.TargetE();
+             ebs.TargetF();
+             ebs.Index();
+             ebs.Wagesminimal();
+             ebs.Wagesmaximal();
+             ebs.Tasks();
+             ebs.Add();
+             ebs.Del();
+             ebs.Changes();
+             ebs.PoOtdelam();
+        }
+    }
+}
